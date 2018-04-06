48cf9522e08124dc699a43dff67a52248040799d
joyjoy9711 <joyjoy97@uw.edu>
2018-04-06 13:12:28 -0700

    ch7_done

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,14 +1,16 @@
 # Exercise 3: vector practice
+chapter 7 Joy Liu 4/5/2018
 
 # Create a vector `words` of 6 (or more) words.
 # You can Google for a "random word generator" if you wish!
-
+words <- c("Joy","is","constantly","learning","and","making","progress")
 
 # Create a vector `words_of_the_day` that is your `words` vector with the string
 # "is the word of the day!" pasted on to the end.
 # BONUS: Surround the word in quotes (e.g., `'data' is the word of the day!`)
 # Note that the results are more obviously correct with single quotes.
 
+words_of_the_day <- paste0("'", words,"'", ' is the word of the day!')
 
 # Create a vector `a_f_words` which are the elements in `words` that start with 
 # "a" through "f"
@@ -16,16 +18,20 @@
 # Tip: make sure all the words are lower-case, and only consider the first letter
 # of the word!
 
+a_f_words <- words[substring(words, 1, 1)<= "f"]
 
 # Create a vector `g_m_words` which are the elements in `words` that start with 
 # "g" through "m"
-
+g_m_words <- words[words >= "g" & substring(words, 1,1)<= "m"]
 
 # Define a function `word_bin` that takes in three arguments: a vector of words, 
 # and two letters. The function should return a vector of words that go between 
 # those letters alphabetically.
+word_bin <- function(words,start,end) {
+  words[words >= start & substring(words,1,1) <= end]
+}
 
 
 # Use your `word_bin` function to determine which of your words start with "e" 
 # through "q"
-
+word_bin(words, "a", "J")
